99ec6413299fbea55dd8960d667ac68bbd8b528e
Alvaro Lamadrid <aglamadrid19@gmail.com>
2021-05-18 00:39:23 -0400

    hard hat config mod

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,20 +1,6 @@
-// require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-truffle5");
 require("@nomiclabs/hardhat-web3");
-
-require('dotenv').config()
-
-const Web3 = require('web3')
-const ContractKit = require('@celo/contractkit')
-
-const web3 = new Web3(process.env.DATAHUB_CELO_TESTNET)
-const kit = ContractKit.newKitFromWeb3(web3)
-
-async function awaitWrapper(){
-  kit.addAccount(process.env.DEV_ACCOUNT_PKEY)
-}
-awaitWrapper()
-
 
 task("accounts", "Prints the list of accounts", async () => {
   const accounts = await ethers.getSigners();
@@ -32,11 +18,7 @@ module.exports = {
   networks: {
     hardhat: {
       allowUnlimitedContractSize: true,
-    },
-    alfajores: {
-      provider: kit.connection.web3.currentProvider, // CeloProvider
-      network_id: 44787                              // Alfajores network id
-    },
+    }
   }
 };
 
